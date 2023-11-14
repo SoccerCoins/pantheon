5597ad6f9cdcb4f9a1967d8894a30437142c5831
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:03:07 +0000

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className="gallery">
+            <h1>Photo Gallery</h1>
+            <p>Explore the beauty and elegance of Pantheon Hotel through our photo gallery.</p>
+            {/* Display a collection of images showing various aspects of the hotel */}
+            {/* Example: <img src="url_to_image" alt="description" /> */}
+        </div>
+    );
+};
+
+export default Gallery;
