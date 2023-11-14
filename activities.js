cd7d595c52fb3c9f1769d7e38306b6eff749b4c1
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:02:46 +0000

    Create activities.js

---
+++ activities.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Activities = () => {
+    return (
+        <div className="activities">
+            <h1>Activities and Entertainment</h1>
+            <p>Engage in a wide range of activities for all ages, from mini-golf to cultural tours.</p>
+            {/* Add descriptions and images of available activities and entertainment options */}
+        </div>
+    );
+};
+
+export default Activities;
