f1c3c9122a730f732d9908025d937838e8b0f072
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 15:12:28 +0000

    Create Rooms.js

---
+++ src/pages/Rooms.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rooms = () => {
+    return (
+        <div className="rooms">
+            <h1>Our Rooms</h1>
+            <p>Explore our range of comfortable and luxurious rooms, each designed to provide an unforgettable stay at Pantheon Hotel.</p>
+            {/* Additional content such as room descriptions, images, amenities can be added here */}
+        </div>
+    );
+};
+
+export default Rooms;
