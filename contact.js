93671545508ecc2065174a9d98713bac2ebe7d35
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:03:32 +0000

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Contact Us</h1>
+            <p>Have questions or need to make a reservation? Reach out to us.</p>
+            {/* Include contact form, address, phone number, and email */}
+            {/* Example of contact details: */}
+            <p>Email: info@pantheon-hotel.gr</p>
+            <p>Phone: +30 123 456 7890</p>
+        </div>
+    );
+};
+
+export default Contact;
