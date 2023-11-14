1b6a98f8706d85841f5b75c08199d058fed32315
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 12:59:51 +0000

    Create home.js

---
+++ home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to Pantheon Hotel</h1>
+            <p>Your perfect getaway in the heart of Crete.</p>
+            {/* Add more content and styling as needed */}
+        </div>
+    );
+};
+
+export default Home;
