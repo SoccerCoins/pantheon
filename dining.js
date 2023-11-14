8bc6e39a32867db782986a4acf3f13797ced9d44
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:02:13 +0000

    Create dining.js

---
+++ dining.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dining = () => {
+    return (
+        <div className="dining">
+            <h1>Dining at Pantheon Hotel</h1>
+            <p>Experience the best of local and international cuisine at our in-house restaurants and bars.</p>
+            {/* Include more detailed descriptions and images of dining areas and menus */}
+        </div>
+    );
+};
+
+export default Dining;
