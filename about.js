439e72327cd0389c536dd813025d31af6a94ed8d
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:00:18 +0000

    Create about.js

---
+++ about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Us</h1>
+            <p>Discover the history and luxury of Pantheon Hotel...</p>
+            {/* Add more detailed content and images */}
+        </div>
+    );
+};
+
+export default About;
