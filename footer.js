1f458d79c09de3f6a360f185775c1d7ce1e0c02d
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 12:59:26 +0000

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Pantheon Hotel</p>
+            <p>Contact Us: info@pantheon-hotel.gr</p>
+        </footer>
+    );
+};
+
+export default Footer;
