590691346a7730bc1df6250a64cafae37cf87729
SoccerCoins <37705089+SoccerCoins@users.noreply.github.com>
2023-11-14 13:00:43 +0000

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rooms = () => {
+    return (
+        <div className="rooms">
+            <h1>Our Rooms</h1>
+            <p>Explore our variety of room options, each tailored for your comfort...</p>
+            {/* Add detailed room descriptions and images */}
+        </div>
+    );
+};
+
+export default Rooms;
